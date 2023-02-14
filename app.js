00925944506d3c2797eac923fba719f28845d785
Amir <iskandar.amir@gmail.com>
2023-02-14 16:33:41 +0800

    Created search endpoint

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ app.get("/top", async (req, res) => {
     await Promise.all(
       posts.data.map(async (post) => {
         const comments = await axios.get(
-          `https://jsonplaceholder.typicode.com/comments?postId=${post.id}`
+          `https://jsonplaceholder.typicode.com/comments`,
+          {
+            params: { postId: post.id },
+          }
         );
         if (comments.data.length > comment_count) {
           comment_count = comments.data.length;
@@ -42,7 +45,31 @@ app.get("/top", async (req, res) => {
 
     res.send(top_posts);
   } catch (error) {
-    console.log(error);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+app.get("/search", async (req, res) => {
+  const { postId, id, name, email, body } = req.query;
+  try {
+    const comments = await axios.get(
+      `https://jsonplaceholder.typicode.com/comments`,
+      {
+        params: {
+          postId: postId,
+          id: id,
+        },
+      }
+    );
+    filtered = comments.data.filter((comment) => {
+      return (
+        comment.name.includes(name) &&
+        comment.email.includes(email) &&
+        comment.body.includes(body)
+      );
+    });
+    res.send(filtered);
+  } catch (error) {
     res.status(500).send("Something went wrong");
   }
 });
